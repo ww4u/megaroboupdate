fba3b739bc0bc4e69788fdbb381bca38f85aa88e
wangzhiyan <wangzhiyan@megarobo.tech>
2019-04-08 13:27:09 +0800

    adb

---
+++ readme.md
@@ -0,0 +1,44 @@
+# MEGAROBO Update升级说明
+## 需求
+* 各个升级部分格式统一
+* 操作方式统一
+
+## 现状
+* MRQ系列后缀 .upd
+* MRH系列后缀 .mrh
+* MRQ升级工具：
+  * ![image.png](https://upload-images.jianshu.io/upload_images/3399573-5e02c0a1342e4f82.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+  * ![image.png](https://upload-images.jianshu.io/upload_images/3399573-ed403bc3d37427ca.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+* MRH升级工具
+   * ![image.png](https://upload-images.jianshu.io/upload_images/3399573-c1a7ae6a0cf200f8.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+* 升级文件内容
+  | 名称 | 内容 | 大小 |
+  | -- | -- | -- |
+  | GreatShot_1.20_Beta1.mrh | | 6.29 MB |
+  |  | ARM|  |
+  |  | FPGA|  |
+  | Sinanju(SOFT)_00.00.01.33.upd | | 906 KB |
+  |  | 背板|  |
+  |  | 主板|  |
+  |  | 内核|  |
+  |  | 设备|  |
+  |  | webcontrol|  |
+  |  | MCT| ? |
+## 集成
+* 升级文件组成
+   * 映射表
+   * entity(实体)
+* 实体组成
+   * 映射表
+   * block(块)
+* 块组成
+   * 类型：描述，fpga,arm,mcu,kernel...
+   * 格式: 原始，压缩
+   * 大小
+   * 验证
+   * 数据
+  
+![image.png](https://upload-images.jianshu.io/upload_images/3399573-d6d0571b4df8d402.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+
+
---
+++ update.mm
@@ -0,0 +1,105 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1554688322962" ID="ID_961417952" MODIFIED="1554688362775" TEXT="update">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1554688363566" HGAP="44" ID="ID_1252624827" MODIFIED="1554689015996" POSITION="right" TEXT="block" VSHIFT="-11">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1554688365275" ID="ID_805201478" MODIFIED="1554688382487" TEXT="type id">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1554688677434" ID="ID_1180477129" MODIFIED="1554688680390" TEXT="description">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554688415819" ID="ID_362852518" MODIFIED="1554688448006" TEXT="fpga">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554688453193" ID="ID_1928364841" MODIFIED="1554688454983" TEXT="arm">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554688456393" ID="ID_1705802882" MODIFIED="1554688457862" TEXT="mcu">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554688485067" ID="ID_921713169" MODIFIED="1554688494694" TEXT="kernel">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554688565963" ID="ID_231974252" MODIFIED="1554688585415" TEXT="mrq_update">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1554688596189" ID="ID_1655635219" MODIFIED="1554688627206" TEXT="Gelgoog.upd"/>
+<node COLOR="#111111" CREATED="1554688615547" ID="ID_824836794" MODIFIED="1554688630616" TEXT="Sinanju.upd"/>
+</node>
+<node COLOR="#990000" CREATED="1554688585642" ID="ID_99073240" MODIFIED="1554688594197" TEXT="mrh_update">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1554688382923" ID="ID_273183000" MODIFIED="1554688398999" TEXT="format">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1554688501020" ID="ID_3293341" MODIFIED="1554688506119" TEXT="raw">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554688506426" ID="ID_1582208142" MODIFIED="1554688512870" TEXT="zip">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1554688399355" ID="ID_1866965207" MODIFIED="1554688400615" TEXT="size">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1554688401098" ID="ID_1577909221" MODIFIED="1554688402551" TEXT="check">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1554688402954" ID="ID_1714664874" MODIFIED="1554688406726" TEXT="payload">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1554688713833" HGAP="50" ID="ID_1923396208" MODIFIED="1554689805404" POSITION="right" TEXT="entity" VSHIFT="-40">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1554688989643" HGAP="23" ID="ID_701650436" MODIFIED="1554689079229" TEXT="entity id" VSHIFT="-7">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1554689097801" ID="ID_625926639" MODIFIED="1554689102007" TEXT="geogoog">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554689102889" ID="ID_1922933710" MODIFIED="1554689109109" TEXT="sinanju">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1554689109577" ID="ID_711985310" MODIFIED="1554689307096" TEXT="mrh_t">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1554688741946" ID="ID_1340153372" MODIFIED="1554688746150" TEXT="block map">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1554688716844" ID="ID_263699498" MODIFIED="1554688723319" TEXT="block1">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1554688723754" ID="ID_919190198" MODIFIED="1554688725206" TEXT="block2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1554688537514" HGAP="-144" ID="ID_1951778048" MODIFIED="1554689790964" POSITION="left" TEXT="conents" VSHIFT="423">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1554688541899" ID="ID_37770997" MODIFIED="1554688913142" TEXT="entity map">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1554688915050" ID="ID_135093421" MODIFIED="1554688919158" TEXT="entity1">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1554688920202" ID="ID_1250465170" MODIFIED="1554688922230" TEXT="entity2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
